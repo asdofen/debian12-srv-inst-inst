d1d3f1be62f516900b96735e2cb8e24fdfa753d6
Dmitriy <42874117+asdofen@users.noreply.github.com>
2024-08-13 11:37:12 +0700

    Add files via upload

---
+++ global-script-debian.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "::: Changing sources :::"
+
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+touch /etc/apt/sources.list
+
+cat << EOF > /etc/apt/sources.list
+deb http://deb.debian.org/debian bookworm main non-free-firmware
+deb-src http://deb.debian.org/debian bookworm main non-free-firmware
+
+deb http://deb.debian.org/debian-security/ bookworm-security main non-free-firmware
+deb-src http://deb.debian.org/debian-security/ bookworm-security main non-free-firmware
+
+deb http://deb.debian.org/debian bookworm-updates main non-free-firmware
+deb-src http://deb.debian.org/debian bookworm-updates main non-free-firmware
+EOF
+
+echo "::: Sources changed :::"
+
+
+echo ::: Turning off Sleep, Hibernate, and Suspend :::
+
+systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+
+echo ::: Installing some necessary apps :::
+
+
+apt update && apt -y upgrade
+apt install -y samba tightvncserver dbus-x11 lxqt pivpn cockpit-client nginx speedtest-cli cbm firefox
+
+
+echo "::: Script done! :::"
